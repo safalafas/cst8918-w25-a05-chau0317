05ade5cdc99167618b8841e42d4ed20b732c63d7
safalafas <safal@ismt.edu.np>
2025-02-07 11:18:40 -0500

    Add Terraform configuration with required providers and runtime settings

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "cloudinit" {}
