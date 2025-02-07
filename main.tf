f778abb47e31c5ca546f5b1e1f3c370df2a64e3e
safalafas <safal@ismt.edu.np>
2025-02-07 11:22:00 -0500

    Add public IP, virtual network, and subnet definitions to main.tf

--- main.tf
+++ main.tf
@@ -41,3 +41,29 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.labelPrefix}-A05-RG"
   location = var.region
 }
+
+
+# Define a public IP address
+resource "azurerm_public_ip" "webserver" {
+  name                = "${var.labelPrefix}A05PublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Define the virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}A05Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Define the subnet
+resource "azurerm_subnet" "webserver" {
+  name                 = "${var.labelPrefix}A05Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
