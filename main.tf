0666f2aa878e03f370d50cc3fd703797d4277f8b
safalafas <safal@ismt.edu.np>
2025-02-07 11:20:12 -0500

    Add configuration variables for resource naming and VM setup

--- main.tf
+++ main.tf
@@ -19,3 +19,20 @@ provider "azurerm" {
 }
 
 provider "cloudinit" {}
+
+# Define config variables
+variable "labelPrefix" {
+  type        = string
+  description = "Your college username. This will form the beginning of various resource names."
+}
+
+variable "region" {
+  default = "canadacentral"
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "azureadmin"
+  description = "The username for the local user account on the VM."
+}
+
